baa505633b4ecc848bb2aeed41979589d4ae1d51
mailrun <mailrun@sohu.com>
2025-03-27 23:58:27 +0800

    code备注-20250327

--- web/.storybook/main.ts
+++ web/.storybook/main.ts
@@ -1,19 +1,22 @@
+// web/.storybook/main.ts 文件是 Storybook 配置文件，用于配置 Storybook 的行为和外观。
+// Storybook 是一个用于开发 UI 组件的环境，它允许你以隔离的方式渲染组件，并查看它们在不同状态下的表现。
+
 import type { StorybookConfig } from '@storybook/nextjs'
 
 const config: StorybookConfig = {
   // stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   stories: ['../app/components/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    '@storybook/addon-onboarding',
+    '@storybook/addon-onboarding', 
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@chromatic-com/storybook',
     '@storybook/addon-interactions',
   ],
   framework: {
-    name: '@storybook/nextjs',
+    name: '@storybook/nextjs',  // 表示 Storybook 将为 Next.js 项目提供特定的支持。
     options: {},
   },
-  staticDirs: ['../public'],
+  staticDirs: ['../public'], // 指定 Storybook 应该加载静态文件的目录
 }
 export default config
--- web/.storybook/preview.tsx
+++ web/.storybook/preview.tsx
@@ -1,3 +1,6 @@
+// 这段代码是使用 React 和 Storybook 进行开发的配置文件。
+// 首先引入了 React 和一些特定的模块，包括Preview类型、withThemeByDataAttribute函数以及自定义的I18nServer组件等，并引入了一些样式文件。
+
 import React from 'react'
 import type { Preview } from '@storybook/react'
 import { withThemeByDataAttribute } from '@storybook/addon-themes'
@@ -6,7 +9,9 @@ import I18nServer from '../app/components/i18n-server'
 import '../app/styles/globals.css'
 import '../app/styles/markdown.scss'
 import './storybook.css'
-
+// 接着定义了一个名为decorators的数组，其中包含了两个装饰器。
+// 第一个装饰器使用withThemeByDataAttribute函数来根据data-theme属性设置主题，提供了 light 和 dark 两种主题，并默认使用 light 主题。
+// 第二个装饰器是一个函数，它接收一个Story组件作为参数，将其包裹在I18nServer组件中返回。
 export const decorators = [
   withThemeByDataAttribute({
     themes: {
@@ -22,7 +27,9 @@ export const decorators = [
     </I18nServer>
   },
 ]
-
+// 最后定义了一个名为preview的对象，它包含了parameters属性，其中的controls对象又有matchers属性，用于指定在 Storybook 的控制界面中对颜色和
+// 日期类型的匹配规则。
+// 最后，将preview对象导出作为默认导出。
 const preview: Preview = {
   parameters: {
     controls: {
