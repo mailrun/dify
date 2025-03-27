c7545df4cabbedfc66d5af02d5e038996adb0ba1
mailrun <mailrun@sohu.com>
2025-03-28 00:34:34 +0800

    20250328-1

--- web/bin/uglify-embed.js
+++ web/bin/uglify-embed.js
@@ -7,3 +7,8 @@ const { readFileSync, writeFileSync } = fs
 writeFileSync('public/embed.min.js', UglifyJS.minify({
   'embed.js': readFileSync('public/embed.js', 'utf8'),
 }).code, 'utf8')
+
+// 这段代码使用 Node.js 的文件系统模块（fs）和 UglifyJS 库来压缩名为 “embed.js” 的文件，并将压缩后的内容写入 “public/embed.min.js” 文件中。
+// 首先引入了 Node.js 的文件系统模块（fs）和 UglifyJS 库。然后，使用 fs 的同步读取文件方法（readFileSync）读取 “public/embed.js” 文件的内容，
+// 并将其作为参数传入 UglifyJS.minify 方法进行压缩。最后，使用 fs 的同步写入文件方法（writeFileSync）将压缩后的代码写入 “public/embed.min.js” 
+// 文件中，编码格式为 “utf8”。
--- web/context/dataset-detail.ts
+++ web/context/dataset-detail.ts
@@ -2,6 +2,10 @@ import { createContext, useContext, useContextSelector } from 'use-context-selec
 import type { DataSet } from '@/models/datasets'
 import type { IndexingType } from '@/app/components/datasets/create/step-two'
 
+// DatasetDetailContextValue 类型定义通常用于在 Typescript 项目中确保对象符合特定的结构。
+// 这种类型定义在使用 React Context、Redux、或任何其他状态管理库时-特别有用-，因为它可以帮助开发者明确每个属性应该是什么类型，
+// 以及哪些属性是可选的。通过使用这种类型定义，可以在编译时捕捉到潜在的错误，比如尝试访问一个不存在的属性，或者将一个错误类型的值赋给一个属性。
+
 type DatasetDetailContextValue = {
   indexingTechnique?: IndexingType
   dataset?: DataSet
